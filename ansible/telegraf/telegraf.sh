dbaeacfb82e5540c23629793dc7c9bc09b9adb0d
quydo <quy196hp@gmail.com>
2018-07-18 13:56:29 +0700

    update code

--- ansible/telegraf/telegraf.sh
+++ ansible/telegraf/telegraf.sh
@@ -7,9 +7,13 @@ enabled = 1
 gpgcheck = 1
 gpgkey = https://repos.influxdata.com/influxdb.key" >> /etc/yum.repos.d/influxdata.repo
 
-sudo yum install telegraf -y 
-mv /etc/telegraf/telegraf.conf /etc/telegraf/telegraf.conf.ori
-cp -r conf/* /etc/telegraf/conf.d/ 
+sudo yum install telegraf -y
+if [ -f /etc/telegraf/telegraf.conf ];then
+  mv /etc/telegraf/telegraf.conf /etc/telegraf/telegraf.conf.ori
+fi
+if [ -d /etc/telegraf/conf.d/];then
+  cp -r conf/* /etc/telegraf/conf.d/
+fi
 cp telegraf.conf /etc/telegraf/
 sudo systemctl enabled telegraf
 sudo systemctl start telegraf
--- install_lemp/mysql57.sh
+++ install_lemp/mysql57.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 wget http://dev.mysql.com/get/mysql57-community-release-el7-7.noarch.rpm
-yum localinstall mysql57-community-release-el7-7.noarch.rpm
+yum localinstall mysql57-community-release-el7-7.noarch.rpm -y
 yum install mysql-community-server -y
 systemctl enable mysqld
 systemctl start mysqld
+cat /var/log/mysqld.log | grep pass
 mysql_secure_installation
