0ec6022556fef6c4b20d6f3b409640b879350914
quydo <quy196hp@gmail.com>
2018-07-18 14:23:49 +0700

    update code

--- ansible/telegraf/telegraf.sh
+++ ansible/telegraf/telegraf.sh
@@ -10,20 +10,22 @@ function hasFile {
 influx_repo_file="/etc/yum.repos.d/influxdata.repo"
 repo_existed=$(hasFile $influx_repo_file)
 if [ "$repo_existed" == "true" ];then
-  echo "[influxdb]
-  name = InfluxData Repository - RHEL \$releasever
-  baseurl = https://repos.influxdata.com/rhel/\$releasever/\$basearch/stable
-  enabled = 1
-  gpgcheck = 1
-  gpgkey = https://repos.influxdata.com/influxdb.key" >> /etc/yum.repos.d/influxdata.repo
+  touch $influx_repo_file
 fi
+echo "[influxdb]
+name = InfluxData Repository - RHEL \$releasever
+baseurl = https://repos.influxdata.com/rhel/\$releasever/\$basearch/stable
+enabled = 1
+gpgcheck = 1
+gpgkey = https://repos.influxdata.com/influxdb.key" > /etc/yum.repos.d/influxdata.repo
+
 sudo yum install telegraf -y
 if [ -f /etc/telegraf/telegraf.conf ];then
   mv /etc/telegraf/telegraf.conf /etc/telegraf/telegraf.conf.ori
 fi
-if [ -d /etc/telegraf/conf.d/ ];then
+if [ -d /etc/telegraf/telegraf.d/ ];then
   cp -r conf/* /etc/telegraf/conf.d/
 fi
 cp telegraf.conf /etc/telegraf/
-sudo systemctl enabled telegraf
+sudo systemctl enable telegraf
 sudo systemctl start telegraf
